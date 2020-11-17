4e287631af64334ccc96cca2b95820c32c3e6557
Nastya <buraya82@gmail.com>
2020-11-18 00:39:53 +0300

    Задание 8 - заполнение полей по умолчанию для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/newcontact/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/newcontact/tests/ContactCreationTests.java
@@ -10,7 +10,7 @@ public class ContactCreationTests extends ContactBase {
   public void testContactCreation() throws Exception {
     app.wd.get("http://localhost/addressbook/delete.php?part=5;");
     app.gotoNewContactPage();
-    app.getContactHelper().fillContactForm(new ContactData("Anastasia", "Verem", "Minsk", "+375298641245", "babaVera@tut.by"));
+    app.getContactHelper().fillContactForm(new ContactData("Anastasia", "Verem", "null", "+375298641245", "null"));
     app.getContactHelper().submitContactCreation();
     app.getContactHelper().returnToHomepage();
     app.Logout();
