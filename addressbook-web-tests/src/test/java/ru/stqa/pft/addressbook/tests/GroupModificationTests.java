b4111391bf54ac802edb6e2d8a241a587e62ea44
Nastya <buraya82@gmail.com>
2020-11-29 23:48:26 +0300

    Задание 9 - спавнение списков для модификации групп (добавление id)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.GroupHelper;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -15,18 +16,23 @@ public class GroupModificationTests extends TestBase {
         GroupHelper groupHelper = app.getGroupHelper();
 
         if (!groupHelper.isThereAGroup()) {
-            groupHelper.createGroup( new GroupData( "test 1", "null", "null" ) );
+            groupHelper.createGroup( new GroupData( "test1", "null", "null" ) );
             }
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = groupHelper.getGroupList();
         groupHelper.selectGroup(before.size() - 1);
         groupHelper.initGroupModification();
-        groupHelper.fillGroupForm(new GroupData( "test 11", "test 12", "test 13" ) );
+        GroupData group = new GroupData(before.get( before.size() - 1 ).getId(), "test1", "test2", "test3" );
+        groupHelper.fillGroupForm( group );
         groupHelper.submitGroupModification();
         app.getNavigationHelper().gotoGroupPage();
 
         List<GroupData> after = groupHelper.getGroupList();
         Assert.assertEquals(after.size(), before.size() );
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals( new HashSet<Object>(before),  new HashSet<Object>(after));
     }
 }
 
