ec8e3adf8bf525fae35b77f7b60bcfa544fd54d9
Nastya <buraya82@gmail.com>
2020-12-02 02:14:01 +0300

    Задание 9 - сортировка списков для Group Creation test

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -13,22 +13,8 @@ public class GroupData {
         this.id=id;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData=(GroupData) o;
-        return id == groupData.id &&
-                Objects.equals( name, groupData.name );
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash( id, name );
-    }
-
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -41,6 +27,18 @@ public class GroupData {
         this.footer = footer;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData=(GroupData) o;
+        return Objects.equals( name, groupData.name );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( name );
+    }
 
     public String getName() { return name;  }
 
