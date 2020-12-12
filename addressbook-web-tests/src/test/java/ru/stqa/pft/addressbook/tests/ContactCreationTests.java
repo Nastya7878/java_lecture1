f4ef7c743642b98fd73dee0a57f191da697e7fbb
Nastya <buraya82@gmail.com>
2020-12-13 01:52:42 +0300

    Задание 12 - вспомогательный тест для поиска файла (6.1)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,24 +5,35 @@ import ru.stqa.pft.addressbook.appmanager.ContactHelper;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
+  @Test (enabled=false)
   public void testContactCreation() {
-
     ContactHelper contactHelper=app.contact();
     Contacts before = contactHelper.all();
     app.gotoNewContactPage();
-    ContactData contact = new ContactData().withSurname( "Verem" ).withFirstname( "Anastasia" ).withAddress( "Minsk" ).withPhone("+375298641245").withEmail("babaVera@tut.by").withGroup( "test1" );
+    File photo = new File("src/test/resources/stru.png");
+    ContactData contact = new ContactData().withSurname( "test_surname" ).withFirstname( "test_name" ).withPhoto( photo );
     contactHelper.create(contact);
     assertThat(app.contact().count(), equalTo(before.size() + 1 ));
     Contacts after = contactHelper.all();
     assertThat( after, equalTo
             (before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))) );
+  }
+
+  @Test
+  public void testCurrentDir() {
+    File currentDir = new File (".");
+    System.out.println(currentDir.getAbsolutePath());
+    File photo = new File("src/test/resources/stru.png");
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
   }
 
 }
