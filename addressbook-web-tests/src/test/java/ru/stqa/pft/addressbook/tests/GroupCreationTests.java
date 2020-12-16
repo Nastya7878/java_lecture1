6affd76ea7209bfdfbdccf3dc93790a4df738f07
Nastya <buraya82@gmail.com>
2020-12-17 01:51:10 +0300

    Задание 12 - параметризация при создании группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,40 +1,50 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.GroupHelper;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list=new ArrayList<Object[]>();
+        list.add( new Object[]{ new GroupData().withName( "test1" ).withHeader( "header 1" ).withFooter( "footer 1" ) } );
+        list.add( new Object[]{ new GroupData().withName( "test2" ).withHeader( "header 2" ).withFooter( "footer 2" ) } );
+        list.add( new Object[]{ new GroupData().withName( "test3" ).withHeader( "header 3" ).withFooter( "footer 3" ) } );
+        return list.iterator();
+    }
 
-    @Test
-    public void testGroupCreation() {
+    @Test(dataProvider="validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         GroupHelper groupHelper=app.group();
         Groups before=groupHelper.all();
-        GroupData group=new GroupData().withName( "test2" );
         groupHelper.create( group );
         assertThat( app.group().count(), equalTo( before.size() + 1 ) );
         Groups after=groupHelper.all();
         assertThat( after.size(), equalTo( before.size() + 1 ) );
-
-
         assertThat( after, equalTo
-                ( before.withAdded(group.withId( after.stream().mapToInt( (g) -> g.getId() ).max().getAsInt() )) ) );
-
+                ( before.withAdded( group.withId( after.stream().mapToInt( (g) -> g.getId() ).max().getAsInt() ) ) ) );
     }
+
     @Test
     public void testBadGroupCreation() {
         app.goTo().groupPage();
         GroupHelper groupHelper=app.group();
         Groups before=groupHelper.all();
-        GroupData group=new GroupData().withName( "test' " );
+        GroupData group=new GroupData().withName( "test" );
         groupHelper.create( group );
-        assertThat( app.group().count(), equalTo( before.size()));
+        assertThat( app.group().count(), equalTo( before.size() ) );
         Groups after=groupHelper.all();
         assertThat( after, equalTo( before));
     }
