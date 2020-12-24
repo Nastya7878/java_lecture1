f47606ca7f829a34f59e1244e058b5e9d49016cd
Nastya <buraya82@gmail.com>
2020-12-24 23:21:26 +0300

    Задание 15 - сравнение данных, загруженных из БД, при удалении Групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,45 +1,38 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.GroupHelper;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Set;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase {
     private int i;
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().groupPage();
-        GroupHelper groupHelper=app.group();
-        if (groupHelper.all().size()== 0) {
+        if (app.db().groups().size() == 0) {
+            app.goTo().groupPage();
+            GroupHelper groupHelper=app.group();
             groupHelper.create( new GroupData().withName( "test1" ) );
         }
     }
 
+
     @Test
     public void testGroupDeletion() {
         GroupHelper groupHelper=app.group();
-        Groups before=groupHelper.all();
+        Groups before = app.db().groups();
         GroupData deletedGroup=before.iterator().next();
+        app.goTo().groupPage();
         groupHelper.delete( deletedGroup );
         assertThat( app.group().count(), equalTo( before.size() -1 ) );
-        Groups after=groupHelper.all();
+        Groups after = app.db().groups();
         assertThat( after, equalTo( before.without( deletedGroup ) ) );
-
     }
-
-
 }
 
 
