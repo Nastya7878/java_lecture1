549688f8eed35353073eafa87debc4c67089ecbb
Nastya <buraya82@gmail.com>
2021-01-03 18:44:20 +0300

    Задание 21 - возможность запуска удаленного браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,21 +1,18 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
-import static org.testng.Assert.assertTrue;
 
 public class ApplicationManager {
     private final Properties properties;
@@ -26,98 +23,71 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private ContactHelper contactHelper;
     private String browser;
-    public boolean acceptNextAlert=true;
+
+
     private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser=browser;
-        properties = new Properties();
+        properties=new Properties();
 
     }
 
     public void init() throws IOException {
         String target=System.getProperty( "target", "local" );
-        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        properties.load( new FileReader( new File( String.format( "src/test/resources/%s.properties", target ) ) ) );
 
-        dbHelper = new DbHelper();
+        dbHelper=new DbHelper();
 
-        if (browser.equals( BrowserType.FIREFOX )) {
-            wd=new FirefoxDriver();
-        } else if (browser.equals( BrowserType.CHROME )) {
-            wd=new ChromeDriver();
-        } else if (browser.equals( BrowserType.IE )) {
-            wd=new InternetExplorerDriver();
+        if ("".equals( properties.getProperty( "selenium.server" ) )) {
+            if (browser.equals( BrowserType.FIREFOX )) {
+                wd=new FirefoxDriver();
+            } else if (browser.equals( BrowserType.CHROME )) {
+                wd=new ChromeDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities=new DesiredCapabilities();
+            capabilities.setBrowserName( browser );
+            wd=new RemoteWebDriver( new URL( properties.getProperty( "selenium.server" ) ), capabilities );
         }
 
-        wd.manage().timeouts().implicitlyWait( 0, TimeUnit.SECONDS );
-        wd.get( properties.getProperty( "web.baseUrl" ));
+
+        wd.manage().timeouts().implicitlyWait( 3, TimeUnit.SECONDS );
+        wd.get( properties.getProperty( "web.baseUrl" ) );
         groupHelper=new GroupHelper( wd );
         contactHelper=new ContactHelper( wd );
         navigationHelper=new NavigationHelper( wd );
         sessionHelper=new SessionHelper( wd );
-        sessionHelper.login(properties.getProperty( "web.adminLogin"), properties.getProperty( "web.adminPassword" ));
+        sessionHelper.login( properties.getProperty( "web.adminLogin" ), properties.getProperty( "web.adminPassword" ) );
     }
 
-    public void Logout() {
-        wd.findElement( By.linkText( "Logout" ) ).click();
-    }
 
     public void stop() {
         wd.quit();
     }
 
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement( by );
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
 
     public GroupHelper group() {
+
         return groupHelper;
     }
 
     public NavigationHelper goTo() {
+
         return navigationHelper;
     }
 
-    public DbHelper db() { return dbHelper;  }
-
-    public void returnHomePage() {
-        wd.findElement( By.xpath( "//div[@id='header']/a" ) ).click();
-    }
-
-    public void gotoNewContactPage() { wd.findElement( By.linkText( "add new" ) ).click(); }
-
-
-
-
-    public String closeAlertAndGetItsText() {
-        try {
-            Alert alert=wd.switchTo().alert();
-            String alertText=alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert=true;
-        }
-    }
-
-    public void AlertAndGetItsText() {
-        assertTrue( closeAlertAndGetItsText().matches( "^Delete 1 addresses[\\s\\S]$" ) );
-    }
-
     public ContactHelper contact() {
-        return new ContactHelper( wd );
+        return contactHelper;
     }
+
+    public DbHelper db() {
+        return dbHelper;
+    }
+
 }
+
+
 
 
 
