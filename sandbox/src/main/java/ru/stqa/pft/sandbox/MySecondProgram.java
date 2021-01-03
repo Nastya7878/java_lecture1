cbcb1f57050c9f0b6e1ffbbea8db277b8cdf3245
Nastya <buraya82@gmail.com>
2021-01-03 19:58:47 +0300

    Задание 2 - расстояние

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class MySecondProgram {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point();
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(4,5);
         p1.x = 1;
         p1.y = 1;
         p2.x = 4;
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,13 +4,22 @@ public class Point {
     public double x;
     public double y;
 
-    public double distance (Point p1) {
+    public Point (double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
-        return Math.sqrt(Math.pow((this.x - p1.x), 2) + Math.pow((this.y - p1.y), 2));
+    public double distance(Point p1) {
 
+        return Math.sqrt( Math.pow( (this.x - p1.x), 2 ) + Math.pow( (this.y - p1.y), 2 ) );
 
     }
-   }
+    public String printPoint() {
+        return this.x + ";" + this.y;
+    }
+}
+
+
 
 
 
