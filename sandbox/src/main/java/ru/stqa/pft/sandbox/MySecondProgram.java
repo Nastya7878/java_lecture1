1f73da04b260283fa8164ab502b5102f3807ad2f
Nastya <buraya82@gmail.com>
2021-01-05 14:19:14 +0300

    задание 2 - исправленное

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MySecondProgram.java
@@ -4,10 +4,7 @@ public class MySecondProgram {
     public static void main(String[] args) {
         Point p1 = new Point(1,1);
         Point p2 = new Point(4,5);
-        p1.x = 1;
-        p1.y = 1;
-        p2.x = 4;
-        p2.y = 5;
+        
 
         System.out.println("Distance between p1 and p2 = " + p1.distance(p2));
     }
